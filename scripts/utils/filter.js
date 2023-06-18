8e201b10bf23e8c429467c80085ba7c32f1ac9f2
lea yabra <yabralea@gmail.com>
2023-06-18 19:54:43 +0200

    version 3

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -14,18 +14,98 @@ const allIngredients = recipes.reduce((ingredients, recipe) => {
 // Générer les éléments HTML pour la liste des ingrédients
 const ingredientsList = document.createElement('div');
 ingredientsList.classList.add('filterlist');
-allIngredients.forEach(ingredient => {
-  const ingredientItem = document.createElement('li');
-  ingredientItem.textContent = ingredient;
-  ingredientsList.appendChild(ingredientItem);
-});
+const selectedIngredients = []; // Tableau pour stocker les éléments déjà sélectionnés
 
+// Générer les éléments HTML pour la liste des ingrédients initiale
+generateFilteredIngredientsList(allIngredients);
 // Ajouter la liste des ingrédients au filtre des ingrédients
 ingredientsFilter.appendChild(ingredientsList);
 
+// Générer les éléments HTML pour la liste des ingrédients
+function generateFilteredIngredientsList(ingredients) {
+  ingredientsList.innerHTML = '';
+  ingredients.forEach(ingredient => {
+    const ingredientItem = document.createElement('li');
+    ingredientItem.textContent = ingredient;
+    addClickEventListener(ingredientItem, ingredient);
+    ingredientsList.appendChild(ingredientItem);
+  });
+}
+
+// Ajout d'un écouteur d'événement sur la liste d'ingredients
+ingredientsFilter.addEventListener('click', searchIngredient);
+// Ajout d'un écouteur d'événement sur ingredients
+ingredientsFilter.addEventListener('input', searchIngredient);
+// Fonction pour effectuer une recherche d'ingrédients
+function searchIngredient() {
+  // Récupération du terme de recherche et nettoyage
+  const searchIngredient = document.querySelector('.filterBtn:nth-child(1) .searchFilter input').value.trim().replace(/\s+/g, ' ').toLowerCase();
+  // Filtrer les ingrédients en fonction du texte de recherche
+  const filterIngredients = allIngredients.filter(ingredient =>
+    ingredient.toLowerCase().includes(searchIngredient)
+  );
+  generateFilteredIngredientsList(filterIngredients);
+}
+
+// Fonction pour créer un tag d'ingrédient
+function createIngredientTag(ingredient) {
+  const ingredientTag = document.createElement('div');
+  ingredientTag.textContent = ingredient;
+  ingredientTag.classList.add('selectedIngredientTag');
+
+  const closeIcon = document.createElement('span');
+  closeIcon.innerHTML = '&times;';
+  closeIcon.classList.add('closeIcon');
+
+  closeIcon.addEventListener('click', () => {
+    ingredientTag.remove();
+    // Retirer l'élément de la liste des éléments sélectionnés
+    const index = selectedIngredients.indexOf(ingredient);
+    if (index > -1) {
+      selectedIngredients.splice(index, 1);
+      updateRecipeByIngredients();
+    }
+  });
+
+  ingredientTag.appendChild(closeIcon);
+  const tag = document.querySelector('.tag');
+  tag.appendChild(ingredientTag);
+}
+
+// Ajouter un écouteur d'événements click à chaque élément de la liste
+function addClickEventListener(ingredientItem, ingredient) {
+  ingredientItem.addEventListener('click', () => {
+    // Vérifier si l'élément a déjà été sélectionné
+    if (!selectedIngredients.includes(ingredient)) {
+      createIngredientTag(ingredient);
+      // Ajouter l'élément à la liste des éléments sélectionnés
+      selectedIngredients.push(ingredient);
+      updateRecipeByIngredients();
+    }
+  });
+}
+
+///recette
+// Fonction pour filtrer les recettes en fonction des ingrédients sélectionnés
+function updateRecipeByIngredients() {
+  const filteredRecipes = recipes.filter(recipe => {
+    // Vérifier si tous les ingrédients sélectionnés sont présents dans la recette
+    return selectedIngredients.every(ingredient => {
+      return recipe.ingredients.some(recipeIngredient => recipeIngredient.ingredient === ingredient);
+    });
+  });
+
+  if (filteredRecipes.length > 0){
+    updateRecipeDisplay(filteredRecipes);
+    updateFilterLists(filteredRecipes);
+    } else {
+      resetRecipeListDisplay(filteredRecipes);
+      resetFilterLists(filteredRecipes);
+  }
+}
+
 // Récupérer la référence de l'élément de filtre des appareils
 const appliancesFilter = document.querySelector('.filterBtn:nth-child(2) .searchFilter');
-
 // Tableau pour stocker les appareils
 const appliances = [];
 
@@ -74,3 +154,66 @@ ustensils.forEach(( ustensil) => {
 // Ajouter la liste des ingrédients au filtre des ustensiles
 ustensilsFilter.appendChild(ustensilsList);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ajout d'un écouteur d'événement sur appareils
+appliancesFilter.addEventListener('input', searchAppliances);
+// Fonction pour effectuer une recherche d'appareils
+function searchAppliances() {
+  // Récupération du terme de recherche et nettoyage
+  const searchAppliances= document.querySelector('.filterBtn:nth-child(2) .searchFilter input').value.trim().replace(/\s+/g, ' ').toLowerCase();
+
+  // Filtrer les appareils en fonction du texte de recherche
+  const filterAppliances = appliances.filter(appliance =>
+    appliance.toLowerCase().includes(searchAppliances)
+  );
+  
+  // Supprimer les anciens éléments de la liste des appareils
+  appliancesList.innerHTML = '';
+  
+  // Générer les nouveaux éléments HTML pour la liste des appareils filtrés
+  filterAppliances.forEach(appliance=> {
+    const applianceItem = document.createElement('li');
+    applianceItem.textContent = appliance;
+    appliancesList.appendChild(applianceItem);
+  });
+}
+
+// Ajout d'un écouteur d'événement sur ustensils
+ustensilsFilter.addEventListener('input', searchUstensils);
+// Fonction pour effectuer une recherche d'ustensils
+function searchUstensils() {
+  // Récupération du terme de recherche et nettoyage
+  const searchUstensils= document.querySelector('.filterBtn:nth-child(3) .searchFilter input').value.trim().replace(/\s+/g, ' ').toLowerCase();
+
+  // Filtrer les ustensils en fonction du texte de recherche
+  const filterUstensils = ustensils.filter(ustensil =>
+    ustensil.toLowerCase().includes(searchUstensils)
+  );
+  
+  // Supprimer les anciens éléments de la liste des ustensils
+  ustensilsList.innerHTML = '';
+  
+  // Générer les nouveaux éléments HTML pour la liste des ustensils filtrés
+  filterUstensils.forEach(ustensil=> {
+    const ustensilItem = document.createElement('li');
+    ustensilItem.textContent = ustensil;
+    ustensilsList.appendChild(ustensilItem);
+  });
+}
